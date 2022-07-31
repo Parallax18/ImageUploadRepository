e248056442be443bda5acd7be99327e4daf1cb0c
Parallax18 <rhythmist204@gmail.com>
2022-07-31 14:10:35 +0100

    first commit

---
+++ src/lib/components/ImageUploadModule.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImageUploadModule = () => {
+  return (
+    <div>ImageUploadModule</div>
+  )
+}
+
+export default ImageUploadModule
---
+++ src/lib/index.js
@@ -0,0 +1,1 @@
+export { default as ImageUploadModule } from './components/ImageUploadModule'
